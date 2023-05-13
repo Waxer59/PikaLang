977b751be77b2c7a702889fd0c7d71ccce2ea81c
waxer59 <78129249+Waxer59@users.noreply.github.com>
2023-05-13 09:51:48 +0200

    feat evaluate binary expressions

--- pkg/interpreter/interpreter.go
+++ pkg/interpreter/interpreter.go
@@ -6,11 +6,63 @@ import (
 	"pika/pkg/interpreter/interpreterValues"
 )
 
+func evaluateBinaryExpr(binop ast.BinaryExpr) interpreterValues.RuntimeValue {
+	lhs := Evaluate(binop.Left)
+	rhs := Evaluate(binop.Right)
+
+	if lhs.GetType() == interpreterValues.Number && rhs.GetType() == interpreterValues.Number {
+		return evaluateNumericBinaryExpr(binop.Operator, lhs, rhs)
+	}
+
+	return interpreterValues.NullVal{Type: "null", Value: "null"}
+}
+
+func evaluateNumericBinaryExpr(operator string, lhs interpreterValues.RuntimeValue, rhs interpreterValues.RuntimeValue) interpreterValues.RuntimeValue {
+	result := 0
+
+	valLhs, okLhs := lhs.(interpreterValues.NumberVal)
+	valRhs, okRhs := rhs.(interpreterValues.NumberVal)
+
+	if !okLhs || !okRhs {
+		panic("Left and right hand side of binary expression must be of type number")
+	}
+
+	switch operator {
+	case "+":
+		result = valLhs.Value + valRhs.Value
+	case "-":
+		result = valLhs.Value - valRhs.Value
+	case "*":
+		result = valLhs.Value * valRhs.Value
+	case "/":
+		//TODO: Division by zero checks
+		result = valLhs.Value / valRhs.Value
+	case "%":
+		result = valLhs.Value % valRhs.Value
+	}
+
+	return interpreterValues.NumberVal{Value: result, Type: interpreterValues.Number}
+}
+
+func evaluateProgram(program ast.Program) interpreterValues.RuntimeValue {
+	var lastEvaluated interpreterValues.RuntimeValue = interpreterValues.NullVal{Type: "null", Value: "null"}
+
+	for _, statement := range program.Body {
+		lastEvaluated = Evaluate(statement)
+	}
+
+	return lastEvaluated
+}
+
 func Evaluate(astNode ast.Stmt) interpreterValues.RuntimeValue {
 	switch astNode.GetKind() {
 	case astTypes.NumericLiteral:
-		value := astNode.(*ast.NumericLiteral).GetValue().(int)
+		value := astNode.(ast.NumericLiteral).GetValue().(int)
 		return interpreterValues.NumberVal{Value: value, Type: "number"}
+	case astTypes.BinaryExpr:
+		return evaluateBinaryExpr(astNode.(ast.BinaryExpr))
+	case astTypes.Program:
+		return evaluateProgram(astNode.(ast.Program))
 	case astTypes.NullLiteral:
 		return interpreterValues.NullVal{Type: "null", Value: "null"}
 	default:
--- pkg/interpreter/interpreterValues/interpreterValues.go
+++ pkg/interpreter/interpreterValues/interpreterValues.go
@@ -7,14 +7,24 @@ const (
 	Number ValueType = "number"
 )
 
-type RuntimeValue interface{}
+type RuntimeValue interface {
+	GetType() ValueType
+}
 
 type NullVal struct {
-	Type  string
+	Type  ValueType
 	Value string
 }
 
+func (n NullVal) GetType() ValueType {
+	return n.Type
+}
+
 type NumberVal struct {
-	Type  string
+	Type  ValueType
 	Value int
+}
+
+func (n NumberVal) GetType() ValueType {
+	return n.Type
 }
