7a6f7874d63012aced98b672c87ffba9fb1b1957
waxer59 <78129249+Waxer59@users.noreply.github.com>
2023-07-25 17:56:05 +0200

    add ast flag

--- pkg/cli/commands/command_run.go
+++ pkg/cli/commands/command_run.go
@@ -22,6 +22,13 @@ func SetUpRunCommand(app *cli.App) *cli.Command {
 		Name:   "run",
 		Usage:  "Run a file",
 		Action: runApp,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "ast",
+				Aliases: []string{"a"},
+				Usage:   "Print the AST",
+			},
+		},
 	}
 
 	return &runCommand
@@ -30,6 +37,9 @@ func SetUpRunCommand(app *cli.App) *cli.Command {
 func runApp(cCtx *cli.Context) error {
 	fileName := cCtx.Args().Get(0)
 	ext := filepath.Ext(fileName)
+
+	isAstActivated := cCtx.Bool("ast")
+
 	if ext != ".pk" && !strings.HasSuffix(fileName, "/") && fileName != "." {
 		return cli.Exit("File extension must be .pk", int(exitCodes.FileExtensionError))
 	}
@@ -61,6 +71,10 @@ func runApp(cCtx *cli.Context) error {
 	parser := parser.New()
 
 	program, err := parser.ProduceAST(src)
+
+	if isAstActivated {
+		fmt.Println(program)
+	}
 
 	if err != nil {
 		return fmt.Errorf(err.Error())
