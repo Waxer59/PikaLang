793558e91b79abf418ebed30cf901094c10ee411
Waxer59 <hnboyano@gmail.com>
2023-09-07 05:35:45 +0200

    add fixes

--- internal/errors/errors_functions.go
+++ internal/errors/errors_functions.go
@@ -1,9 +1,11 @@
 package compilerErrors
 
 const (
-	ErrFuncNotFound          = "ERROR: Function not found: "
-	ErrFuncExpectedIdentifer = "ERROR: Expected identifier for function name"
-	ErrReturn                = "ERROR: Return statement outside of function"
-	ErrNotEnoughArguments    = "ERROR: Not enough arguments for function"
-	ErrTooManyArguments      = "ERROR: Too many arguments for function"
+	ErrFuncNotFound                 = "ERROR: Function not found: "
+	ErrFuncExpectedIdentifer        = "ERROR: Expected identifier for function name"
+	ErrReturn                       = "ERROR: Return statement outside of function"
+	ErrNotEnoughArguments           = "ERROR: Not enough arguments for function"
+	ErrTooManyArguments             = "ERROR: Too many arguments for function"
+	ErrUndefinedFunction            = "ERROR: Undefined function: "
+	ErrComputedPropertyMustBeString = "ERROR: Computed property must be a string"
 )
